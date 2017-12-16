fa1e864d4f863c415ad64f9f91c13c8ae45b07b8
Sam Taylor <sam.taylor92@hotmail.com>
2017-12-16 10:54:02 +0000

    Some refactoring and exception handling, also removed the token and moved it into an unversioned .txt file for security ;)

--- GDBot.py
+++ GDBot.py
@@ -5,22 +5,22 @@ from discord.ext import commands
 import platform
 
 # Here you can modify the bot's prefix and description and whether it sends help in direct messages or not.
-client = Bot(description="GDBot by King-Pie#8803", command_prefix="!", pm_help=True)
+bot = Bot(description="GDBot by King-Pie#8803", command_prefix="!", pm_help=True)
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count
 # the bot is connected to, and the bot id in the console.
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as ' + client.user.name + ' (ID:' + client.user.id + ') | Connected to ' + str(
-        len(client.servers)) + ' servers | Connected to ' + str(len(set(client.get_all_members()))) + ' users')
+    print('Logged in as ' + bot.user.name + ' (ID:' + bot.user.id + ') | Connected to ' + str(
+        len(bot.servers)) + ' servers | Connected to ' + str(len(set(bot.get_all_members()))) + ' users')
     print('--------')
     print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__,
                                                                                platform.python_version()))
     print('--------')
-    print('Use this link to invite {}:'.format(client.user.name))
-    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+    print('Use this link to invite {}:'.format(bot.user.name))
+    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(bot.user.id))
     print('--------')
     print('Github Link: https://github.com/King-Pie/GDBot')
     print('Based on Basic Bot by Habchy#1665 - https://github.com/Habchy/BasicBot/')
@@ -28,14 +28,22 @@ async def on_ready():
     print('Created by King-Pie#8803')
 
 
-# This is a basic example of a call and response command. You tell it do "this" and it does it.
-@client.command()
+@bot.command()
 async def ping(*args):
-    await client.say(":ping_pong: Pong!")
+    """ Pong! """
+    await bot.say(":ping_pong: Pong!")
 
 
-@client.command()
-async def stupid(user):
-    await client.say(user + ' is stupid')
+@bot.command()
+async def stupid(user=None):
+    """ Says that a user is stupid """
+    try:
+        await bot.say("@" + str(user) + " is stupid!")
+    except Exception:
+        await bot.say("Who's stupid?")
 
-client.run('MzkxMjYwMDE5MjM4MjQwMjU2.DRWEzQ.-LnRCtdmH5p42xJYCQvt-G4q91I')
+token_file = open("token", "r")
+token = token_file.readline()
+token_file.close()
+
+bot.run(str(token))
